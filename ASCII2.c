29b26f2d5932792d9699bbbfb2b8fbab9aa483fb
PrincePoudel-Chess <prince1234poudel@gmail.com>
2025-02-20 19:54:32 +0545

    Add files via upload

---
+++ ASCII2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+
+void main(){
+
+    char a ;
+    scanf("%c",&a);
+
+        if(a>='A'&&a<='Z'){
+                printf("The character is alphabet.");}
+        else if(a>='a' && a<='z'){
+            printf("Character is an alphabet.");
+
+        }
+        else if(a>='0'&&a<='9'){printf("The character is a Number.");}
+
+        else if((a>'!' && a<'/')){printf("The character is a special character.");}
+        else if((a>':' && a<'"')){printf("The character is a special character.");}
+        else if((a>'[' && a<'`')){printf("The character is a special character.");}
+        else if((a>'{' && a<='~')){printf("The character is a special character.");}
+
+
+
+
+
+
+
+
+return 0;
+getch();
+
+
+
+}
