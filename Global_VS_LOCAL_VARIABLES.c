c8d49809e098db4c1c304d967680c190e2ac758d
PrincePoudel-Chess <prince1234poudel@gmail.com>
2025-02-20 21:21:26 +0545

    Add files via upload

---
+++ Global_VS_LOCAL_VARIABLES.c
@@ -0,0 +1,35 @@
+//program to demonstrate example of global and local scope
+
+#include<stdio.h>
+
+
+//We Define the Global Variable outside the functions
+//We Define the Local Variable inside the function
+float pie = 3.14;
+
+
+int main(){
+    int a = 3;
+    printf("The Global Variable is :%f",pie);
+    printf("\nThe local Variable is a whose Value is : %d",a);
+    getch();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
