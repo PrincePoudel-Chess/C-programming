28057e945af094f02782f7d2283e6d4ad6071c9f
PrincePoudel-Chess <prince1234poudel@gmail.com>
2025-02-20 20:14:58 +0545

    Add files via upload

---
+++ FibonacciSeries.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int a = 0;
+    int b = 1;
+    int c= 0;
+    int n;
+    printf("Enter the number of terms: "); scanf("%d",&n);
+    for(int i = 0;i<n;i++){
+        printf("%d ",a);
+        c = a+b;
+        b = a;
+        a= c;
+
+
+    }
+    getch();
+    return 0;
+
+
+
+}
