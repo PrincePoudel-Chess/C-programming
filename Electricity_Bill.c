a5f350d2ba86c191e47be80cde199131de8440d5
PrincePoudel-Chess <prince1234poudel@gmail.com>
2025-02-20 20:06:26 +0545

    Add files via upload

---
+++ Electricity_Bill.c
@@ -0,0 +1,23 @@
+//Finding the electricity Bill
+
+#include<stdio.h>
+
+int main(){
+    float units,cost;
+    printf("Enter the units consumed:");
+    scanf("%f",&units);
+    if(units<=20){
+        printf("The total Cost:Rs%d",80);
+    }
+    else if(units>20 && units <=120){
+        cost = 80 + 7.30*(units-20);
+        printf("Total Cost = %f",cost);
+    }
+    else{
+        cost = 80 + 7.30 * 100 + 9 * (units-120);
+        printf("Total Cost = %f",cost);}
+
+    getch();
+    return 0 ;
+
+}
