ca5491fbc4139c2bd338c358323a9463830227fd
PrincePoudel-Chess <prince1234poudel@gmail.com>
2025-02-20 19:52:23 +0545

    Add files via upload

---
+++ 2digits_and_rev.c
@@ -0,0 +1,20 @@
+//Write a program that asks a number and test the number whether it
+//is multiple of 5 or not, divisible by 7 but not by eleven.
+
+#include<stdio.h>
+
+
+int main(){
+    int a;
+    printf("Enter the  Number:");
+    scanf("%d",&a);
+    if((a%5==0 && a%7 == 0)&&(a%11 != 0)){printf("Multiple of 5 and Divisible by 7 and Not divisible by 11.");}
+    else if(a%5 !=0&& (a%7!= 0)&&(a%11!= 0)){printf("Not a Multiple of 5.");}
+
+    else if((a%5 ==0) && (a%7!= 0)&&(a%11 != 0)){printf(" Multiple of 5 but not divisible by 7.");}
+    else if(a%5 !=0&& (a%7 == 0)&&(a%11!= 0)){printf("Not a Multiple of 5 but divisible by 7.");}
+
+
+
+
+}
