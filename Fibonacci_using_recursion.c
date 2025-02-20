7ecfc43abfd8fedbb937a894a149a4a1efff804c
PrincePoudel-Chess <prince1234poudel@gmail.com>
2025-02-20 20:14:32 +0545

    Add files via upload

---
+++ Fibonacci_using_recursion.c
@@ -0,0 +1,30 @@
+
+////0 1 1 2 3 5 8.....
+
+
+#include<stdio.h>
+int fibo(int a,int b,int n){
+    int temp;
+    printf("%d ",a);
+    temp = b;
+    b = a;
+    a = temp;
+    n = n-1;
+    if(n>1){return fibo(a+b,b,n);}
+
+
+
+
+
+
+
+
+}
+
+int main(){int n=15;
+    fibo(0,1,n);
+
+
+
+
+}
